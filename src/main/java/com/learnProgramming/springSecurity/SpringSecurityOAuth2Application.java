1b58c46dde25c0f30cf2279f8023e99ff79d1037
Naga sahithreddy Yannam <nagasahithreddyyannam@Nagas-MacBook-Pro.local>
2021-01-10 21:15:40 -0700

    Spring Security oAuth2

---
+++ src/main/java/com/learnProgramming/springSecurity/SpringSecurityOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.learnProgramming.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnProgramming/springSecurity/SpringSecurityOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnProgramming.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
